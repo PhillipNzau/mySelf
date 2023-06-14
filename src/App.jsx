b515c67fbc2fa45842d33a5998f5b4f48c23228c
phillipnzau <nzauonline78@gmail.com>
2023-06-14 12:16:21 +0300

    Font add

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,8 @@ function App() {
       }`}
     >
       <Header onToggle={toggleMenu} isMenuOpen={isMenuOpen} />
-      <CardContainer />
+
+      {!isMenuOpen && <CardContainer />}
     </div>
   );
 }
